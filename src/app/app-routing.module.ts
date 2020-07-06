7d631d31bfc6af2440373cd81bef16daeabe7639
Petar Jovanovic <Petar.Jovanovic@airvanpovanovic.home>
2020-07-06 22:05:33 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
+import { WelcomeComponent } from './welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: '/', component: WelcomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
